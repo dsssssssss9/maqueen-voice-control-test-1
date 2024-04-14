91fdcf29efc270686985cd0ba676d3c7748926c6
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2024-04-14 19:52:59 +0100

    update com codes to match

--- main.ts
+++ main.ts
@@ -1,20 +1,30 @@
 radio.onReceivedNumber(function (receivedNumber) {
-	
+    cmd = receivedNumber
+    if (cmd == 62) {
+        basic.showIcon(IconNames.Happy)
+    } else if (cmd == 65) {
+        basic.clearScreen()
+    } else if (cmd == 103) {
+        maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
+        maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
+    } else if (cmd == 104) {
+        maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
+        maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
+    }
 })
 input.onButtonPressed(Button.A, function () {
     maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
     maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
 })
-radio.onReceivedString(function (receivedString) {
-	
-})
 input.onButtonPressed(Button.B, function () {
     maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
     maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
 })
+let cmd = 0
 radio.setGroup(1)
 maqueen.motorStop(maqueen.Motors.All)
-basic.showIcon(IconNames.Heart)
+cmd = 999
+basic.showIcon(IconNames.Chessboard)
 basic.forever(function () {
 	
 })
