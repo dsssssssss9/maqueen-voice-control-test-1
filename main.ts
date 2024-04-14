f5010dfcce0ccc312f25556f67645914370311d4
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2024-04-14 17:47:59 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+radio.onReceivedNumber(function (receivedNumber) {
+	
+})
+radio.onReceivedString(function (receivedString) {
+	
+})
+radio.setGroup(1)
+maqueen.motorStop(maqueen.Motors.All)
+basic.showIcon(IconNames.Heart)
+basic.forever(function () {
+	
+})
