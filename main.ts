e42ce052b611e2cb957e0b31859964b2892d329d
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2024-04-14 18:31:59 +0100

    add left right led test

--- main.ts
+++ main.ts
@@ -1,8 +1,16 @@
 radio.onReceivedNumber(function (receivedNumber) {
 	
 })
+input.onButtonPressed(Button.A, function () {
+    maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
+    maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
+})
 radio.onReceivedString(function (receivedString) {
 	
+})
+input.onButtonPressed(Button.B, function () {
+    maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
+    maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
 })
 radio.setGroup(1)
 maqueen.motorStop(maqueen.Motors.All)
